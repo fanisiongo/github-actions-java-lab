8b0ce7ebe10011b2c61617a4f92753b7c1e31826
fanisiongo <118754589+fanisiongo@users.noreply.github.com>
2026-08-08 17:26:27 +0900

    Add HelloWorld application

---
+++ src/main/java/com/github/fanisiongo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.github.fanisiongo;
+
+public final class HelloWorld {
+    private HelloWorld() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello, GitHub Actions!");
+    }
+}
